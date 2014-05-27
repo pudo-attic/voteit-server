d7d5300a2309fcd6ddf2dc8fc1e3d357564602e9
Friedrich Lindenberg <friedrich@pudo.org>
2014-05-27 14:11:34 +0200

    Vote events CRUD.

--- voteit/web.py
+++ voteit/web.py
@@ -1,10 +1,10 @@
-from voteit.core import app, motions
+from voteit.core import app, motions, vote_events
 from voteit.util import jsonify, paginate_cursor, obj_or_404
 
 
 @app.route('/api/1')
 @app.route('/')
-def index():
+def api_index():
     return jsonify({'status': 'ok'})
 
 
@@ -17,5 +17,20 @@ def motions_index():
 
 @app.route('/api/1/motions/<object_id>')
 def motions_get(object_id):
-    data = obj_or_404(motions.find_one({'object_id': object_id}))
+    obj = motions.find_one({'object_id': object_id})
+    obj = obj_or_404(obj)
+    return jsonify(obj)
+
+
+@app.route('/api/1/vote_events')
+def vote_events_index():
+    cur = vote_events.find({}, {'votes': False})
+    data = paginate_cursor(cur)
     return jsonify(data)
+
+
+@app.route('/api/1/vote_events/<identifier>')
+def vote_events_get(identifier):
+    obj = vote_events.find_one({'identifier': identifier})
+    obj = obj_or_404(obj)
+    return jsonify(obj)
